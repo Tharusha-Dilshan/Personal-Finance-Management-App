5e142ac51fe53bc19036c9789e85a24f87b6333d
Nisal <it21259302@my.sliit.lk>
2023-05-09 15:15:53 +0530

    start image upload part in create account

--- app/src/main/java/com/example/personal_finance_management_app/CreateAccount.kt
+++ app/src/main/java/com/example/personal_finance_management_app/CreateAccount.kt
@@ -1,19 +1,29 @@
 package com.example.personal_finance_management_app
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.personal_finance_management_app.DataClasses.User
 import com.example.personal_finance_management_app.databinding.ActivityCreateAccountBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 
 class CreateAccount : AppCompatActivity() {
     private lateinit var binding: ActivityCreateAccountBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+    private var imageUri : Uri? =null
+
+    private val selectImage = registerForActivityResult(ActivityResultContracts.GetContent()){
+        imageUri =it
+        binding.homeAvatarImg.setImageURI(imageUri)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityCreateAccountBinding.inflate(layoutInflater)
@@ -23,13 +33,19 @@ class CreateAccount : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
 
+        binding.homeAvatarImg.setOnClickListener{
+            selectImage.launch("image/*")
+
+        }
+
+
         binding.SingInButton.setOnClickListener {
             val name = binding.etName.text.toString()
             val email = binding.etEmail.text.toString()
             val password = binding.etPass.text.toString()
             val confirmPwd = binding.etConPass.text.toString()
 
-            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPwd.isEmpty()) {
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPwd.isEmpty() ) {
                 if (name.isEmpty()) {
                     binding.etName.error = "Enter your name"
                 }
@@ -57,38 +73,70 @@ class CreateAccount : AppCompatActivity() {
             } else if (confirmPwd != password) {
                 binding.etConPass.error = "Passwords do not match. Please try again."
             } else {
-//                Toast.makeText(this, "All good", Toast.LENGTH_SHORT).show()
-                //creating an auth for the user
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
 
-                    if (it.isSuccessful) {
-                        //store user details in the database
-                        val databaseRef =
-                            database.reference.child("users").child(auth.currentUser!!.uid)
-                        val user: User = User(name, email, auth.currentUser!!.uid)
-                        databaseRef.setValue(user).addOnCompleteListener {
+                //uploadImage()
+
+                /* val storageRef = FirebaseStorage.getInstance().getReference("users")
+                    .child(auth.currentUser!!.uid).child("users.jpg")
+
+
+                storageRef.putFile(imageUri!!).addOnSuccessListener {
+                    storageRef.downloadUrl.addOnSuccessListener {
+                               val image = it.toString() */
+
+                        //Toast.makeText(this, "All good", Toast.LENGTH_SHORT).show()
+                        //creating an auth for the user
+                        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+
                             if (it.isSuccessful) {
-                                //redirect user to login activity
-                                val intent = Intent(this, Login::class.java)
-                                startActivity(intent)
+                                //store user details in the database
+                                val databaseRef =
+                                    database.reference.child("users").child(auth.currentUser!!.uid)
+                                val user: User = User(name, email, auth.currentUser!!.uid )
+                                databaseRef.setValue(user).addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        //redirect user to login activity
+                                        val intent = Intent(this, Login::class.java)
+                                        startActivity(intent)
+                                    } else {
+                                        Toast.makeText(
+                                            this,
+                                            "Something went wrong, try again",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+
                             } else {
-                                Toast.makeText(
-                                    this,
-                                    "Something went wrong, try again",
-                                    Toast.LENGTH_SHORT
-                                ).show()
+                                Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT)
+                                    .show()
                             }
                         }
 
-                    } else {
-                        Toast.makeText(this, "Something went wrong, try again", Toast.LENGTH_SHORT)
-                            .show()
+
+
+               /*
+                    }.addOnFailureListener{
+                        Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
                     }
+                }.addOnFailureListener{
+                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
                 }
+                */
+
+
+
+
+
+
             }
 
 
         }
 
     }
+
+
+
+
 }
--- app/src/main/java/com/example/personal_finance_management_app/DataClasses/User.kt
+++ app/src/main/java/com/example/personal_finance_management_app/DataClasses/User.kt
@@ -4,4 +4,5 @@ data class User (
     var name: String? = null,
     var email: String? = null,
     var uid: String? = null,
+    var image : String? = "",
 )
