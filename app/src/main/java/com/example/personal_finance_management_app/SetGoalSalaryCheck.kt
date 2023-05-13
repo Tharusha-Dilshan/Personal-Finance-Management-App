615c510b997bb91cf9c4b625533153bdff408d07
PiyumiNadeeshani <piyumiabeykoon1619@gmail.com>
2023-05-14 01:42:50 +0530

    salary part

---
+++ app/src/main/java/com/example/personal_finance_management_app/SetGoalSalaryCheck.kt
@@ -0,0 +1,11 @@
+package com.example.personal_finance_management_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetGoalSalaryCheck : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_goal_salary_check)
+    }
+}
